6380741b9184fc366e080c7e3af3ba107ca53c0f
Sammy J <samueljean09@gmail.com>
2017-06-30 17:47:52 +0000

    finished product

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-/* global $ */
 $(document).ready(function(){
+// $("#myBtn").click(function() {
 
-$( "button" ).click(function() {
-    var myname = document.getElementById('NameEntered').value;
-    var myemail = document.getElementById('EmailEntered').value;
-    $("#dank").append(' Hi ' + myname + 'thank you for signing up! We will send an email to'+ myemail + ' shortly'
-    );
 // Get the modal
 var modal = document.getElementById('myModal');
-
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
-
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
 // When the user clicks on the button, open the modal 
 btn.onclick = function() {
+    var nameen = $('#NameEntered').val();
+ console.log(nameen);
+ var mail = $('#EmailEntered').val();
+ console.log(mail);
+    $("#kek").append('Hello ' + nameen + ' thank you for signing up! We will send an email to '+ mail + ' shortly.'
+   );
     modal.style.display = "block";
 }
+
+// 
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
@@ -30,8 +30,5 @@ window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
-}    
-
-});
-
+}  
 });
